8c3ec97fcd79454121e2baa107d25a3fe9d0e8a0
yinghuali <yinghua.li@uni.lu>
2023-08-21 18:46:36 +0200

    update

---
+++ models/start_get_models.sh
@@ -0,0 +1,2 @@
+python vgg.py --path_x '../data/cifar10_x.pkl' --path_y '../data/cifar10_y.pkl' --num_classes 10 --epochs 10 --batch_size 64  --path_save './target_models/cifa10_vgg_10.pkl'
+
--- models/vgg.py
+++ models/vgg.py
@@ -1,6 +1,7 @@
 import os
 import argparse
 import pickle
+import tensorflow as tf
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
 from tensorflow.keras.layers import Dropout
@@ -16,13 +17,15 @@ os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 ap = argparse.ArgumentParser()
 ap.add_argument("--path_x", type=str)
 ap.add_argument("--path_y", type=str)
+ap.add_argument("--num_classes", type=int)
 ap.add_argument("--epochs", type=int)
 ap.add_argument("--batch_size", type=int)
-ap.add_argument("--path_save", type=int)
+ap.add_argument("--path_save", type=str)
 args = ap.parse_args()
 
 path_x = args.path_x
 path_y = args.path_y
+num_classes = args.num_classes
 epochs = args.epochs
 batch_size = args.batch_size
 path_save = args.path_save
@@ -117,20 +120,22 @@ def vgg(x_train, num_classes):
     return model
 
 
-def main(path_x, path_y, epochs, batch_size, path_dave):
+def main():
     x = pickle.load(open(path_x, 'rb'))
     y = pickle.load(open(path_y, 'rb'))
-
+    x = x.astype('float32')
     x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=0)
     x_train /= 255.0
     x_test /= 255.0
+    y_train = tf.keras.utils.to_categorical(y_train, num_classes)
+    y_test = tf.keras.utils.to_categorical(y_test, num_classes)
 
-    model = vgg(x_train, num_classes=10)
+    model = vgg(x_train, num_classes=num_classes)
     model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs, validation_data=(x_test, y_test), shuffle=True)
 
     scores = model.evaluate(x_test, y_test, verbose=0)
     print("Accuracy: %.2f%%" % (scores[1] * 100))
-    model.save(path_dave)
+    model.save(path_save)
 
 
 if __name__ == '__main__':
