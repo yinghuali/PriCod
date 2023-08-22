6f9fcdb1b2b75aedbffd704f8955014ea6cb1629
yinghuali <yinghua.li@uni.lu>
2023-08-22 22:18:47 +0200

    update

--- get_data.py
+++ get_data.py
@@ -1,10 +1,8 @@
-import numpy as np
-import pickle
-from tensorflow.keras.datasets import cifar10
 import os
 import numpy as np
 import cv2
 import pickle
+from tensorflow.keras.datasets import cifar10, fashion_mnist
 
 
 def get_path_list(path_dir_compile):
@@ -26,20 +24,12 @@ def get_cifar10():
     pickle.dump(cifar10_y, open('./data/cifar10_y.pkl', 'wb'), protocol=4)
 
 
-def get_cifar100():
-    (x_train, y_train), (x_test, y_test) = cifar10.load_data()
-    cifar100_x = np.vstack((x_train, x_test))
-    cifar100_y = np.vstack((y_train, y_test))
-    pickle.dump(cifar100_x, open('./data/cifar100_x.pkl', 'wb'), protocol=4)
-    pickle.dump(cifar100_y, open('./data/cifar100_y.pkl', 'wb'), protocol=4)
-
-
 def get_imagenet(path_data_dir):
     path_list = get_path_list(path_data_dir)
     label_list = [i.split('/train/')[-1].split('/')[0].strip() for i in path_list]
     label_set = list(set(label_list))
     dic = dict(zip(label_set, range(len(label_set))))
-    label_np = np.array([dic[i] for i in label_list])
+    label_np = np.array([[dic[i]] for i in label_list])
     img_np = []
     for i in path_list:
         img = cv2.imread(i)
@@ -53,12 +43,22 @@ def get_imagenet(path_data_dir):
     pickle.dump(label_np, open('./data/imagenet_y.pkl', 'wb'), protocol=4)
 
 
-def main():
-    # get_cifar10()
-    # get_cifar100()
-    # get_imagenet('./data/imagenet/tiny-imagenet-200/train/')
-    pass
+def get_fashionMNIST():
+    (x_train, y_train), (x_test, y_test) = fashion_mnist.load_data()
+    x_train = x_train.reshape((60000, 28, 28, 1))
+    x_test = x_test.reshape((10000, 28, 28, 1))
+    y_train = np.array([[i] for i in y_train])
+    y_test = np.array([[i] for i in y_test])
+    fashionMnist_x = np.vstack((x_train, x_test))
+    fashionMnist_y = np.vstack((y_train, y_test))
+    pickle.dump(fashionMnist_x, open('./data/fashionMnist_x.pkl', 'wb'), protocol=4)
+    pickle.dump(fashionMnist_y, open('./data/fashionMnist_y.pkl', 'wb'), protocol=4)
 
+
+def main():
+    get_cifar10()
+    get_imagenet('./data/imagenet/tiny-imagenet-200/train/')
+    get_fashionMNIST()
 
 
 if __name__ == '__main__':
