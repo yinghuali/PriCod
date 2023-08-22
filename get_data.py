017063a3b49319f6302e5a4d439b79890746b4ca
yinghuali <yinghua.li@uni.lu>
2023-08-22 20:14:40 +0200

    update

--- get_data.py
+++ get_data.py
@@ -1,6 +1,21 @@
 import numpy as np
 import pickle
-from tensorflow.keras.datasets import cifar10, cifar100
+from tensorflow.keras.datasets import cifar10
+import os
+import numpy as np
+import cv2
+import pickle
+
+
+def get_path_list(path_dir_compile):
+    model_path_list = []
+    if os.path.isdir(path_dir_compile):
+        for root, dirs, files in os.walk(path_dir_compile, topdown=True):
+            for file in files:
+                file_absolute_path = os.path.join(root, file)
+                if file_absolute_path.endswith('.JPEG'):
+                    model_path_list.append(file_absolute_path)
+    return model_path_list
 
 
 def get_cifar10():
@@ -19,10 +34,31 @@ def get_cifar100():
     pickle.dump(cifar100_y, open('./data/cifar100_y.pkl', 'wb'), protocol=4)
 
 
+def get_imagenet(path_data_dir):
+    path_list = get_path_list(path_data_dir)
+    label_list = [i.split('/train/')[-1].split('/')[0].strip() for i in path_list]
+    label_set = list(set(label_list))
+    dic = dict(zip(label_set, range(len(label_set))))
+    label_np = np.array([dic[i] for i in label_list])
+    img_np = []
+    for i in path_list:
+        img = cv2.imread(i)
+        img_np.append(img)
+    img_np = np.array(img_np)
+    img_np = img_np.astype(np.int8)
+    print(img_np.shape)
+    print(label_np.shape)
+
+    pickle.dump(img_np, open('./data/imagenet_x.pkl', 'wb'), protocol=4)
+    pickle.dump(label_np, open('./data/imagenet_y.pkl', 'wb'), protocol=4)
+
+
 def main():
     # get_cifar10()
     # get_cifar100()
+    # get_imagenet('./data/imagenet/tiny-imagenet-200/train/')
     pass
+
 
 
 if __name__ == '__main__':
