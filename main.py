27c3a75d128f45f68685a3b6441a9806eef43445
yinghuali <yinghua.li@uni.lu>
2023-08-24 14:52:57 +0200

    update

--- main.py
+++ main.py
@@ -23,11 +23,6 @@ path_save_res = args.path_save_res
 
 # python main.py --path_original_out_vec './models/original_out_vec/cifa10_vgg_20_orginal_vec.pkl' --path_onDevice_out_vec './models/onDevice_out_vec/cifa10_vgg_20_tflite_vec.pkl' --path_embedding_vec './models/embedding_vec/cifar10_embedding.pkl' --path_y './data/cifar10_y.pkl' --path_save_res './results/original/cifa10_vgg_20_tflite_20.json'
 
-# path_original_out_vec = './models/original_out_vec/cifa10_vgg_20_orginal_vec.pkl'
-# path_onDevice_out_vec = './models/onDevice_out_vec/cifa10_vgg_20_tflite_vec.pkl'
-# path_embedding_vec = './models/embedding_vec/cifar10_embedding.pkl'
-# path_y = './data/cifar10_y.pkl'
-# path_save_res = './results/original/cifa10_vgg_20.pkl'
 
 original_out_vec = pickle.load(open(path_original_out_vec, 'rb'))
 onDevice_out_vec = pickle.load(open(path_onDevice_out_vec, 'rb'))
