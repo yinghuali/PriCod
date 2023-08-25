ca7bbdeb7b4e2a9fbee144a29edf2b6df599b104
yinghuali <yinghua.li@uni.lu>
2023-08-26 00:38:31 +0200

    update

--- models/start_get_tflite_probability_vec.sh
+++ models/start_get_tflite_probability_vec.sh
@@ -1,3 +1,12 @@
+#!/bin/bash -l
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --time=1-23:00:00
+#SBATCH --mail-type=end,fail
+#SBATCH --mail-user=yinghua.li@uni.lu
+#SBATCH --output=/dev/null
+#SBATCH -p batch
+#SBATCH --mem 10G
 
 python get_tflite_probability_vec.py --path_model './onDevice_models/cifa10_vgg_20.tflite' --path_x '../data/cifar10_x.pkl' --path_save './onDevice_out_vec/cifa10_vgg_20_tflite_vec.pkl'
 python get_tflite_probability_vec.py --path_model './onDevice_models/cifa10_alexnet_35.tflite' --path_x '../data/cifar10_x.pkl' --path_save './onDevice_out_vec/cifa10_alexnet_35_tflite_vec.pkl'
