9d05429e87371b36a3ca90743050b5adfd12471d
yinghuali <yinghua.li@uni.lu>
2023-08-28 23:49:01 +0200

    update

---
+++ models/start_get_coreml_adv_probability_vec.sh
@@ -0,0 +1,23 @@
+python get_coreml_probability_vec.py --path_model './onDevice_models/cifa10_vgg_20.mlmodel' --model_name 'vgg' --path_x '../advdata/cifar10/bim_x_adv.pkl' --path_save './onDevice_adv_out_vec_coreml/cifa10_vgg_20_bim_x_adv.pkl'
+python get_coreml_probability_vec.py --path_model './onDevice_models/cifa10_vgg_20.mlmodel' --model_name 'vgg' --path_x '../advdata/cifar10/fsgm_x_adv.pkl' --path_save './onDevice_adv_out_vec_coreml/cifa10_vgg_20_fsgm_x_adv.pkl'
+python get_coreml_probability_vec.py --path_model './onDevice_models/cifa10_vgg_20.mlmodel' --model_name 'vgg' --path_x '../advdata/cifar10/patch_x_adv.pkl' --path_save './onDevice_adv_out_vec_coreml/cifa10_vgg_20_patch_x_adv.pkl'
+python get_coreml_probability_vec.py --path_model './onDevice_models/cifa10_vgg_20.mlmodel' --model_name 'vgg' --path_x '../advdata/cifar10/pgd_x_adv.pkl' --path_save './onDevice_adv_out_vec_coreml/cifa10_vgg_20_pgd_x_adv.pkl'
+
+
+python get_coreml_probability_vec.py --path_model './onDevice_models/cifa10_alexnet_35.mlmodel' --model_name 'alexnet' --path_x '../advdata/cifar10/bim_x_adv.pkl' --path_save './onDevice_adv_out_vec_coreml/cifa10_alexnet_35_bim_x_adv.pkl'
+python get_coreml_probability_vec.py --path_model './onDevice_models/cifa10_alexnet_35.mlmodel' --model_name 'alexnet' --path_x '../advdata/cifar10/fsgm_x_adv.pkl' --path_save './onDevice_adv_out_vec_coreml/cifa10_alexnet_35_fsgm_x_adv.pkl'
+python get_coreml_probability_vec.py --path_model './onDevice_models/cifa10_alexnet_35.mlmodel' --model_name 'alexnet' --path_x '../advdata/cifar10/patch_x_adv.pkl' --path_save './onDevice_adv_out_vec_coreml/cifa10_alexnet_35_patch_x_adv.pkl'
+python get_coreml_probability_vec.py --path_model './onDevice_models/cifa10_alexnet_35.mlmodel' --model_name 'alexnet' --path_x '../advdata/cifar10/pgd_x_adv.pkl' --path_save './onDevice_adv_out_vec_coreml/cifa10_alexnet_35_pgd_x_adv.pkl'
+
+
+python get_coreml_probability_vec.py --path_model './onDevice_models/fmnist_lenet1_3.mlmodel' --model_name 'lenet1' --path_x '../advdata/fashionMnist/bim_x_adv.pkl' --path_save './onDevice_adv_out_vec_coreml/fmnist_lenet1_3_bim_x_adv.pkl'
+python get_coreml_probability_vec.py --path_model './onDevice_models/fmnist_lenet1_3.mlmodel' --model_name 'lenet1' --path_x '../advdata/fashionMnist/fsgm_x_adv.pkl' --path_save './onDevice_adv_out_vec_coreml/fmnist_lenet1_3_fsgm_x_adv.pkl'
+python get_coreml_probability_vec.py --path_model './onDevice_models/fmnist_lenet1_3.mlmodel' --model_name 'lenet1' --path_x '../advdata/fashionMnist/patch_x_adv.pkl' --path_save './onDevice_adv_out_vec_coreml/fmnist_lenet1_3_patch_x_adv.pkl'
+python get_coreml_probability_vec.py --path_model './onDevice_models/fmnist_lenet1_3.mlmodel' --model_name 'lenet1' --path_x '../advdata/fashionMnist/pgd_x_adv.pkl' --path_save './onDevice_adv_out_vec_coreml/fmnist_lenet1_3_pgd_x_adv.pkl'
+
+
+python get_coreml_probability_vec.py --path_model './onDevice_models/fmnist_lenet5_3.mlmodel' --model_name 'lenet5' --path_x '../advdata/fashionMnist/bim_x_adv.pkl' --path_save './onDevice_adv_out_vec_coreml/fmnist_lenet5_3_bim_x_adv.pkl'
+python get_coreml_probability_vec.py --path_model './onDevice_models/fmnist_lenet5_3.mlmodel' --model_name 'lenet5' --path_x '../advdata/fashionMnist/fsgm_x_adv.pkl' --path_save './onDevice_adv_out_vec_coreml/fmnist_lenet5_3_fsgm_x_adv.pkl'
+python get_coreml_probability_vec.py --path_model './onDevice_models/fmnist_lenet5_3.mlmodel' --model_name 'lenet5' --path_x '../advdata/fashionMnist/patch_x_adv.pkl' --path_save './onDevice_adv_out_vec_coreml/fmnist_lenet5_3_patch_x_adv.pkl'
+python get_coreml_probability_vec.py --path_model './onDevice_models/fmnist_lenet5_3.mlmodel' --model_name 'lenet5' --path_x '../advdata/fashionMnist/pgd_x_adv.pkl' --path_save './onDevice_adv_out_vec_coreml/fmnist_lenet5_3_pgd_x_adv.pkl'
+
