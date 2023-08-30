13af444f52f8536244c2d28e09e4620066bd59d6
yinghuali <yinghua.li@uni.lu>
2023-08-30 20:46:47 +0200

    update

--- feature_importance.py
+++ feature_importance.py
@@ -25,22 +25,29 @@ path_save_res = args.path_save_res
 # python main.py --path_original_out_vec './models/original_out_vec/cifa10_vgg_20_orginal_vec.pkl' --path_onDevice_out_vec './models/onDevice_out_vec/cifa10_vgg_20_tflite_vec.pkl' --path_embedding_vec './models/embedding_vec/cifar10_embedding.pkl' --path_y './data/cifar10_y.pkl' --path_save_res './results/original/cifa10_vgg_20_tflite.json'
 
 
-original_out_vec = pickle.load(open(path_original_out_vec, 'rb'))
-onDevice_out_vec = pickle.load(open(path_onDevice_out_vec, 'rb'))
-embedding_vec = pickle.load(open(path_embedding_vec, 'rb'))
-
-
 def main():
+    original_out_vec = pickle.load(open(path_original_out_vec, 'rb'))
+    onDevice_out_vec = pickle.load(open(path_onDevice_out_vec, 'rb'))
+    embedding_vec = pickle.load(open(path_embedding_vec, 'rb'))
+
     y = pickle.load(open(path_y, 'rb'))
     y = np.array([i[0] for i in y])
     onDevice_pre_y = onDevice_out_vec.argmax(axis=1)
 
     distance_feature = get_all_feature(original_out_vec, onDevice_out_vec)
-    n_diff = len(distance_feature[0])
-    pca = PCA(n_components=n_diff)
+    # pca = PCA(n_components=100)
+    pca = PCA(n_components=300) # vgg16
+    # pca = PCA(n_components=700)
     new_embedding_vec = pca.fit_transform(embedding_vec)
 
     concat_all_feature = np.hstack((distance_feature, new_embedding_vec))
+
+    # invalid_rows = np.isnan(concat_all_feature).any(axis=1) | np.isinf(concat_all_feature).any(axis=1)
+    # concat_all_feature = concat_all_feature[~invalid_rows]
+    # y= y[~invalid_rows]
+    # onDevice_pre_y = onDevice_pre_y[~invalid_rows]
+    # onDevice_out_vec = onDevice_out_vec[~invalid_rows]
+
 
     target_train_pre, target_test_pre, train_y, test_y = train_test_split(onDevice_pre_y, y, test_size=0.3, random_state=0)
     concat_train_all_feature, concat_test_all_feature, _, _ = train_test_split(concat_all_feature, y, test_size=0.3, random_state=0)
