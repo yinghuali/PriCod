68ab3be16e8c0b24b5ffee59f39aeb7ced8180ca
yinghuali <yinghua.li@uni.lu>
2023-08-30 17:07:13 +0200

    update

--- conver2tflite_test.py
+++
@@ -1,7 +0,0 @@
-import tensorflow as tf
-
-converter = tf.compat.v1.lite.TFLiteConverter.from_keras_model_file(model_file='lenet5.h5')
-converter.post_training_quantize = True
-
-tflite_model = converter.convert()
-open('lenet5.tflite', "wb").write(tflite_model)
