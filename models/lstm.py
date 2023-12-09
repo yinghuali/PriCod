84ea7388efbcca9ce1007893a85db68315598c3b
yinghuali <yinghua.li@uni.lu>
2023-12-09 18:27:47 +0100

    update

---
+++ models/lstm.py
@@ -0,0 +1,8 @@
+import numpy as np
+import pandas as pd
+import pickle
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Embedding, LSTM, Dense
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from sklearn.model_selection import train_test_split
